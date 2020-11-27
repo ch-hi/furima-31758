488023889dd478c1049db64ebaa3a690e470bb1c
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-11-27 15:43:41 +0900

    DB保存されない問題解消
    passwordにバリデーションかけてしまっていたので削除（コメントアウト）

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   #まとめ記法あり参照：https://master.tech-camp.in/curriculums/5389
-  validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }   #半角英数必須
+  # validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }   #半角英数必須encrypted_
   validates :familyname, :firstname, :familykana,:firstkana,:birth, presence: true        
   validates :familyname, :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }             #全角（漢字・ひらがな・カタカナ）での入力が必須であること
   validates :familykana, :firstkana, format: { with: /\p{katakana}/ }  #全角（カタカナ）必須
