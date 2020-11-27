3a5ae9541aff3dd13da988462311a3804ec26754
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-11-27 18:26:24 +0900

    バリデーションにカラムの記述漏れ
    :nickname,　を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   
   #まとめ記法あり参照：https://master.tech-camp.in/curriculums/5389
   # validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }   #半角英数必須encrypted_
-  validates :familyname, :firstname, :familykana,:firstkana,:birth, presence: true        
+  validates :familyname, :firstname, :familykana,:firstkana,:birth,:nickname, presence: true        
   validates :familyname, :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/ }             #全角（漢字・ひらがな・カタカナ）での入力が必須であること
   validates :familykana, :firstkana, format: { with: /\p{katakana}/ }  #全角（カタカナ）必須
 
