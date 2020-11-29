fbdd26bdd3739b46292cd8af3851ca58d1a6eec0
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-11-29 16:54:33 +0900

    item_paramsの中身変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image,:name,:describe,:category_id,:category_id,:fee_id,:area_id,:day_id)
   end  
 end
