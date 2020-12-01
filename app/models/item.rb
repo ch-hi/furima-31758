634de04025efb4711739416b2b8cfee82a203dfd
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-01 12:44:55 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   belongs_to :day
 
   #空の投稿を保存できないようにする
-  validates :name, :describe, :payment, presence: true
+  validates :name, :describe, :payment,:image, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
   validates :category_id,:condition_id,:fee_id,:area_id,:day_id, numericality: { other_than: 1 } 
