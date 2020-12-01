d68e2d11d78063862ff01e8dfdbf4e263a090334
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-01 11:31:35 +0900

    販売価格のjs実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function(){
+  const textField = document.getElementById("item-price");
+  const priceContent = document.getElementById("add-tax-price");
+  const profitContent = document.getElementById("profite");
+
+  // 入力するたびにイベント発火できる,入力した金額の値を取得する
+  textField.addEventListener("input", () => {
+    const inputValue = textField.value;
+    priceContent.innerHTML = Math.floor(inputValue*0.1);
+  
+    const profitContent = document.getElementById("profit");
+    profitContent.innerHTML = Math.floor(inputValue-(inputValue*0.1));
+  
+  })
+})
