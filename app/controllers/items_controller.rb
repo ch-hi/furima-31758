a8c48c90276ee827de44781d44da4aa3a8571608
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-03 14:53:44 +0900

    updateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,6 +29,7 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
+    # redirect_to "/items/#{item.id}"  # コメントと結びつくツイートの詳細画面に遷移する
   end
   
   private
