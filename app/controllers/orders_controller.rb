b5f2d5036810fc4395f4c791fed15a8d8f9dc9a5
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-05 22:41:54 +0900

    PAY.JPを利用したクレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,15 @@
 class OrdersController < ApplicationController
   def index
   end
+
+  def create
+    @order = Order.new(order_params)
+  end
+
+  # private
+
+  # def order_params
+  #   params.permit(:price, :token)
+  # end
+
 end
--- app/javascript/payjp.js
+++ app/javascript/payjp.js
@@ -1,36 +1,41 @@
+//イベント発火
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  const form = document.getElementById("charge-form");
+  const form = document.getElementById("buy-red-btn");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
  
-    const formResult = document.getElementById("charge-form");
+    //カード情報の取得先
+    const formResult = document.getElementById("buy-red-btn");
     const formData = new FormData(formResult);
  
     const card = {
-      number: formData.get("number"),
-      cvc: formData.get("cvc"),
-      exp_month: formData.get("exp_month"),
-      exp_year: `20${formData.get("exp_year")}`,
+      number: formData.get("number"),  // カード番号
+      cvc: formData.get("cvc"),        // カード裏面の3桁の数字
+      exp_month: formData.get("exp_month"),      // 有効期限の月
+      exp_year: `20${formData.get("exp_year")}`, // 有効期限の年
     };
- 
+    
+    //カードオブジェクトをPAY.JPに送信
     Payjp.createToken(card, (status, response) => {
       if (status === 200) {
         const token = response.id;
-        const renderDom = document.getElementById("charge-form");
+        const renderDom = document.getElementById("buy-red-btn");
         const tokenObj = `<input value=${token} type="hidden" name='card_token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
  
+        //カード情報がparamsに含まれないようにする記述
         document.getElementById("number").removeAttribute("name");
         document.getElementById("cvc").removeAttribute("name");
         document.getElementById("exp_month").removeAttribute("name");
         document.getElementById("exp_year").removeAttribute("name");
- 
-        document.getElementById("charge-form").submit();
-        document.getElementById("charge-form").reset();
+        
+        //トークンをコントローラーに送信
+        document.getElementById("buy-red-btn").submit();
+        document.getElementById("buy-red-btn").reset();
       }
     });
   });
  };
- 
+ //これまで記述した関数を全て「定数pay」に代入
  window.addEventListener("load", pay);
