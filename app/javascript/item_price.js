289a1114d8c9cedea2317ade1fdf42b1b358c2ca
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-09 23:46:40 +0900

    PAYJP_JS読み込み確認
    既存記述はコメントアウト

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,22 @@
-window.addEventListener('load', function(){
-  const textField = document.getElementById("item-price");
-  const priceContent = document.getElementById("add-tax-price");
-  const profitContent = document.getElementById("profite");
+const pay = () => {
+  console.log("カード情報トークン化のためのJavaScript");
+};
 
-  // 入力するたびにイベント発火できる,入力した金額の値を取得する
-  textField.addEventListener("input", () => {
-    const inputValue = textField.value;
-    priceContent.innerHTML = Math.floor(inputValue*0.1);
+window.addEventListener("load", pay);
+
+
+// window.addEventListener('load', function(){
+//   const textField = document.getElementById("item-price");
+//   const priceContent = document.getElementById("add-tax-price");
+//   const profitContent = document.getElementById("profite");
+
+//   // 入力するたびにイベント発火できる,入力した金額の値を取得する
+//   textField.addEventListener("input", () => {
+//     const inputValue = textField.value;
+//     priceContent.innerHTML = Math.floor(inputValue*0.1);
   
-    const profitContent = document.getElementById("profit");
-    profitContent.innerHTML = Math.floor(inputValue-(inputValue*0.1));
+//     const profitContent = document.getElementById("profit");
+//     profitContent.innerHTML = Math.floor(inputValue-(inputValue*0.1));
   
-  })
-})
+//   })
+// })
