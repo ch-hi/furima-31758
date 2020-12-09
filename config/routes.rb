eadf2afb4868de1a64aa50df8a59236a9b6e49bb
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-09 22:51:34 +0900

    Formオブジェクト_ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items
-  
+  resources :items do
+    resources :buys
+  end
   # root to: 'orders#index'
-  resources :orders
+  
 end
