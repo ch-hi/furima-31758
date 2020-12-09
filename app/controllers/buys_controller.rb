42fda32fed4ecd992fdde051c4d1328f6db7f6e5
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-10 02:09:09 +0900

    PAYJP_リファクタリング
    処理を移動した

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -7,19 +7,14 @@ class BuysController < ApplicationController
 
  
   def create
-    binding.pry
+    # binding.pry
     @item = Item.find(params[:item_id])
     # binding.pry(params no nakami) => token ga haitteiru => ok
     @buy_form = BuyForm.new(buy_params)   #「BuyForm」に編集
     # binding.pry
      if @buy_form.valid?
-      Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: buy_params[:price],  # 商品の値段
-        card: buy_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
-       @buy_form.save
+      pay_item
+      @buy_form.save
        redirect_to root_path
      else
        render action: :index
@@ -29,6 +24,15 @@ class BuysController < ApplicationController
   private
   def buy_params
    params.permit(:item_id,:posta, :area, :city, :banchi, :building, :phonenumber, :price).merge(user_id: current_user.id,token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+        amount: buy_params[:price],  # 商品の値段
+        card: buy_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
   end
   
   end
