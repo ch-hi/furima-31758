2ba618922b15f3f6e122907007094d27dc74a46c
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-10 19:57:02 +0900

    バリデーションの修正

--- app/models/buy_form.rb
+++ app/models/buy_form.rb
@@ -4,11 +4,15 @@ class BuyForm
   attr_accessor :user_id, :item_id, :posta, :area, :city, :banchi, :building, :phonenumber, :buy_id, :token
   
   
-  validates :token, :posta, :area, :city, :banchi, :phonenumber, presence: true
+  validates :user_id, :item_id, :token, :posta, :area, :city, :banchi, :phonenumber, presence: true
   #郵便番号にはハイフンが必要であること（123-4567となる）
   validates :posta, format: { with: /\A\d{3}[-]\d{4}\z/ }
   #電話番号にはハイフンは不要で、11桁以内であること（09012345678となる）
   validates :phonenumber, format: { with: /\A\d{11}\z/ }
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :area, numericality: { other_than: 1 } 
+
+  validates :phonenumber, presence: true, length: { maximum: 11 }
   
 
 
