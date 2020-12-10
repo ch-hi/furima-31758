e125ca9a1c1dadda6c1cede73259284e5cbb1681
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-10 18:11:31 +0900

    出品機能の金額入力js復旧

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,37 +1,15 @@
-const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); // PAY.JPテスト公開鍵
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    const formResult = document.getElementById("charge-form");
-    const formData = new FormData(formResult);
+window.addEventListener('load', function(){
+  const textField = document.getElementById("item-price");
+  const priceContent = document.getElementById("add-tax-price");
+  const profitContent = document.getElementById("profite");
 
-    const card = {
-      number: formData.get("buy_form[number]"),
-      cvc: formData.get("buy_form[cvc]"),
-      exp_month: formData.get("buy_form[month]"),
-      exp_year: `20${formData.get("buy_form[year]")}`,
-    };
-    console.log(card)
-
-    Payjp.createToken(card, (status, response) => {
-      if (status == 200) {
-        const token = response.id;
-        // console.log(token)　
-        const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
-        renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        // debugger;
-      }
-      document.getElementById("card-number").removeAttribute("name");
-      document.getElementById("card-cvc").removeAttribute("name");
-      document.getElementById("card-exp-month").removeAttribute("name");
-      document.getElementById("card-exp-year").removeAttribute("name");
-    
-      document.getElementById("charge-form").submit();
-    });
-  });
-};
-
-window.addEventListener("load", pay);
+  // 入力するたびにイベント発火できる,入力した金額の値を取得する
+  textField.addEventListener("input", () => {
+    const inputValue = textField.value;
+    priceContent.innerHTML = Math.floor(inputValue*0.1);
+  
+    const profitContent = document.getElementById("profit");
+    profitContent.innerHTML = Math.floor(inputValue-(inputValue*0.1));
+  
+  })
+})
