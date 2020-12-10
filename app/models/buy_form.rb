ac3414e7a7f67739de7e176039dc2a6c34703fe4
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-10 17:28:51 +0900

    バリデーション

--- app/models/buy_form.rb
+++ app/models/buy_form.rb
@@ -2,12 +2,9 @@ class BuyForm
 
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :posta, :area, :city, :banchi, :building, :phonenumber, :buy_id, :token
-
-  #カード情報のバリデーション未実装 token入る
-  validates :price, presence: true
-  validates :token, presence: true
   
-  validates :posta, :area, :city, :banchi, :phonenumber, presence: true
+  
+  validates :token, :posta, :area, :city, :banchi, :phonenumber, presence: true
   #郵便番号にはハイフンが必要であること（123-4567となる）
   validates :posta, format: { with: /\A\d{3}[-]\d{4}\z/ }
   #電話番号にはハイフンは不要で、11桁以内であること（09012345678となる）
@@ -17,9 +14,7 @@ class BuyForm
 
 def save
   buy = Buy.create(user_id: user_id, item_id: item_id)
-  # binding.pry
   Receiver.create(posta: posta, area: area, city: city, banchi: banchi, building: building, phonenumber: phonenumber, buy_id: buy.id)
-  
 end
 
     
