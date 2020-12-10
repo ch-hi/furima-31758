625a60f703a7c6b6c6ea0ac51b7822f15bb97480
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-12-10 17:27:00 +0900

    テストコード実装

---
+++ spec/models/buy_form_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe BuyForm, type: :model do
+  describe '購入情報の保存' do
+    before do
+      @buy_form = FactoryBot.build(:buy_form)
+    end
+
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@buy_form).to be_valid
+    end
+
+    #空だと〜〜できない
+    it 'tokenが空だと保存できないこと' do
+      @buy_form.token = nil
+      @buy_form.valid?
+      expect(@buy_form.errors.full_messages).to include("Token can't be blank")
+    end
+    it 'postaが空だと保存できないこと' do
+      @buy_form.posta= nil
+      @buy_form.valid?
+      expect(@buy_form.errors.full_messages).to include("Posta can't be blank")
+    end
+    it 'areaが空だと保存できないこと' do
+      @buy_form.area = nil
+      @buy_form.valid?
+      expect(@buy_form.errors.full_messages).to include("Area can't be blank")
+    end   
+    it 'cityが空だと保存できないこと' do
+      @buy_form.city = nil
+      @buy_form.valid?
+      expect(@buy_form.errors.full_messages).to include("City can't be blank")
+    end    
+    it 'banchiが空だと保存できないこと' do
+      @buy_form.banchi = nil
+      @buy_form.valid?
+      expect(@buy_form.errors.full_messages).to include("Banchi can't be blank")
+    end    
+    it 'phonenumberが空だと保存できないこと' do
+      @buy_form.phonenumber = nil
+      @buy_form.valid?
+      expect(@buy_form.errors.full_messages).to include("Phonenumber can't be blank")
+    end
+
+
+    #郵便番号
+    it 'postaが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+      @buy_form.posta = '1234567'
+      @buy_form.valid?
+      expect(@buy_form.errors.full_messages).to include("Posta is invalid")
+    end
+    
+    #電話番号
+    it "phonenumberが12桁以上であれば登録できないこと" do
+      @buy_form.phonenumber = "123454567890123"
+      @buy_form.valid?
+      expect(@buy_form.errors.full_messages).to include("Phonenumber is invalid")
+    end
+
+  end
+end
